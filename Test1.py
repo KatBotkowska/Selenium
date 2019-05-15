b2bf3e3633fb4cba347cad49dd9ed98e3fe8d345
katar <katarzyna.botkowska@gmail.com>
2019-05-15 19:56:44 +0200

    próa próba

--- Test1.py
+++ Test1.py
@@ -6,9 +6,9 @@ driver = webdriver.Chrome(executable_path=r'C:\TestFiles\chromedriver.exe')
 
 driver.set_page_load_timeout(10)
 driver.get('http://google.com')
-driver.find_element_by_name('q').send_keys('Eurowizja')
+driver.find_element_by_name('q').send_keys('Selenium')
 
-
+    
 time.sleep(4)
 driver.quit()
 
